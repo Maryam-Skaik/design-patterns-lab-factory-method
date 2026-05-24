0243eafb02e674b96cf2b63d9994f05fade1252a
Maryam Skaik <mariamskaik2003@gmail.com>
2026-05-24 19:17:13 +0300

    Update FileProcessor.java

--- solutions/cloud-file-processing/FileProcessor.java
+++ solutions/cloud-file-processing/FileProcessor.java
@@ -4,7 +4,6 @@ package FileProcessing;
  * FileProcessor interface defines the common behavior
  * for all file processing types.
  * 
- * Key teaching point:
  * - Factory Method works with this abstraction polymorphically.
  * - Clients depend on this interface, not concrete processors.
  */
